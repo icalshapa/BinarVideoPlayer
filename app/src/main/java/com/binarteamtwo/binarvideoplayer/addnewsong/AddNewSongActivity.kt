b452f333b021a2923ed227bcb65257a5bea5bd80
icalshapa <icalshapa@gmail.com>
2021-07-12 18:17:51 +0700

    Initial commit

---
+++ app/src/main/java/com/binarteamtwo/binarvideoplayer/addnewsong/AddNewSongActivity.kt
@@ -0,0 +1,12 @@
+package com.binarteamtwo.binarvideoplayer.addnewsong
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.binarteamtwo.binarvideoplayer.R
+
+class AddNewSongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_song)
+    }
+}
