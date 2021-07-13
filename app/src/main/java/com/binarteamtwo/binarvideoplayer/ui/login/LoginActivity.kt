ba9fb1f46da8d96bc0fa8f0836a0d2d29daacf1a
ertelek <rengga.kade@gmailcom>
2021-07-14 02:32:56 +0700

    Add Splash Screen Add Login Menu
    note : Animation in splash screan not working

---
+++ app/src/main/java/com/binarteamtwo/binarvideoplayer/ui/login/LoginActivity.kt
@@ -0,0 +1,38 @@
+package com.binarteamtwo.binarvideoplayer.ui.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AnimationUtils
+import com.binarteamtwo.binarvideoplayer.R
+import com.binarteamtwo.binarvideoplayer.databinding.ActivityLoginBinding
+import com.binarteamtwo.binarvideoplayer.databinding.ActivitySplashScreenBinding
+import com.binarteamtwo.binarvideoplayer.startAnimation
+import com.binarteamtwo.binarvideoplayer.ui.main.MainActivity
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        onClick()
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.logo_anim).apply {
+            interpolator = AccelerateDecelerateInterpolator()
+        }
+        binding.ivLogoLogin.startAnimation(animation) {}
+
+    }
+
+    private fun onClick() {
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
+}
