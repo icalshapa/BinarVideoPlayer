5411097122644ae4eb66122a0b95f71cbce035d6
icalshapa <icalshapa@gmail.com>
2021-07-16 13:23:28 +0700

    Finished PlayerActivity.kt

--- app/src/main/java/com/binarteamtwo/binarvideoplayer/ui/player/PlayerContract.kt
+++ app/src/main/java/com/binarteamtwo/binarvideoplayer/ui/player/PlayerContract.kt
@@ -1,4 +1,30 @@
 package com.binarteamtwo.binarvideoplayer.ui.player
 
+import com.binarteamtwo.binarvideoplayer.base.BaseContract
+import com.irfan.binarvideoplayer.model.MediaPlaylist
+
 interface PlayerContract {
+    interface View: BaseContract.BaseView{
+
+        //callback when get video
+        fun onFetchVideoSuccess(mediaPlaylist: MediaPlaylist)
+        fun onFetchVideoFailed()
+
+        //callback when change favorite status
+        fun onChangeFavoriteStatusSuccess(mediaPlaylist: MediaPlaylist)
+        fun onChangeFavoriteStatusFailed()
+
+        //set data to layout
+        fun bindVideoData(mediaPlaylist: MediaPlaylist?)
+
+        //getting data
+        fun getData()
+    }
+
+    interface Presenter : BaseContract.BasePresenter {
+        fun getVideo(videoId: Int)
+        fun changeStatusFavorite(mediaPlaylist: MediaPlaylist)
+    }
+
+
 }
